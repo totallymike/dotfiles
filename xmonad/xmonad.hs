ee523711902e2f9173c0aec7266e04c35000b460
Michael Westbom <totallymike@gmail.com>
2013-01-26 11:46:35 -0500

    Added layout names and some workspace hooks.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,21 +3,25 @@ import XMonad
 import XMonad.Actions.GridSelect
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Layout.IM
+import XMonad.Hooks.ManageHelpers
+import XMonad.ManageHook
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
+import qualified XMonad.StackSet as W
+
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig
-    { manageHook = manageDocks <+> manageHook defaultConfig
+    { manageHook = manageDocks <+> myManageHook
     , layoutHook = avoidStruts  $  layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
                     { ppOutput = hPutStrLn xmproc
                     , ppTitle = xmobarColor "green" "" . shorten 50
                     }
     , terminal = "urxvtc"
+    , workspaces = myWorkspaces
     } `additionalKeysP`
     [ ("M-g", goToSelected myGSConfig)
     ]
@@ -25,3 +29,11 @@ main = do
 myGSConfig = defaultGSConfig
   { gs_font = "xft:Source Code Pro:size=10"
   }
+
+myManageHook = composeAll
+  [ className =? "XSassm"    --> doHideIgnore
+  , className =? "SAS"       --> doF (W.shift "sas")
+  ]
+  <+> manageHook defaultConfig
+
+myWorkspaces = ["term", "web", "notes", "sas"] ++ map show [5..9]
